d0f478886c7a55aafaf2d07d02d1ced24f20614f
yagowb <yagodev3@gmail.com>
2023-06-26 09:22:47 -0300

    configuração MongoDB + ajustes de rotas + fix função Inserir

--- src/app.js
+++ src/app.js
@@ -1,99 +1,99 @@
 const express = require('express');
-const mysql = require('mysql');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
-
-// Configuração do servidor
 const app = express();
 const port = 3001;
 
-
-// Configuração do banco de dados
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'seu_usuario',
-  password: 'sua_senha',
-  database: 'seu_banco_de_dados',
-});
-
-
-// Conexão
-connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados: ' + err.stack);
-    return;
-  }
-  console.log('Conexão bem-sucedida ao banco de dados.');
-});
-
-
-// Middleware para processar requisições com corpo em formato JSON
 app.use(express.json());
+app.use(cors());
+
+const uri = 'mongodb+srv://yagodev:m5BGDYPisj4H1Mw4@cluster0.1wafvok.mongodb.net/gerenciadorBiblioteca';
 
 
-// Rota para adicionar um livro
-app.post('/livros', (req, res) => {
-  const { titulo, autor, ano } = req.body;
 
-  if (!titulo || !autor || !ano) {
-    return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
+async function run() {
+  const client = new MongoClient(uri, {
+
+  });
+
+  try {
+    await client.connect();
+    console.log('Conexão bem-sucedida ao banco de dados.');
+    
+
+
+    // Rota para adicionar um livro
+    app.post('/livros', async (req, res) => {
+      const { titulo, autor, ano } = req.body;
+    
+      if (!titulo || !autor || !ano) {
+        return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
+      }
+    
+      const collection = client.db('gerenciadorBiblioteca').collection('Livros');
+      const livro = { titulo, autor, ano };
+    
+      try {
+        await collection.insertOne(livro);
+        return res.status(201).json({ message: 'Livro inserido com sucesso.' });
+      } catch (error) {
+        console.error('Erro ao inserir o livro:', error);
+        return res.status(500).json({ error: 'Erro ao inserir o livro.' });
+      }
+    });
+    
+
+
+    // Rota para buscar um livro
+    app.get('/livros/:titulo', async (req, res) => {
+      const titulo = req.params.titulo;
+    
+      const collection = client.db('gerenciadorBiblioteca').collection('Livros');
+    
+      try {
+        const livro = await collection.findOne({ titulo });
+        if (!livro) {
+          return res.status(404).json({ error: 'Livro não encontrado.' });
+        }
+        return res.json(livro);
+      } catch (error) {
+        console.error('Erro ao buscar o livro:', error);
+        return res.status(500).json({ error: 'Erro ao buscar o livro.' });
+      }
+    });
+
+
+    
+    // Rota para remover um livro
+    app.delete('/livros/:titulo', async (req, res) => {
+      const titulo = req.params.titulo;
+    
+      const collection = client.db('gerenciadorBiblioteca').collection('Livros');
+    
+      try {
+        const result = await collection.deleteOne({ titulo });
+        if (result.deletedCount === 0) {
+          return res.status(404).json({ error: 'Livro não encontrado.' });
+        }
+        return res.json({ message: 'Livro removido com sucesso.' });
+      } catch (error) {
+        console.error('Erro ao remover o livro:', error);
+        return res.status(500).json({ error: 'Erro ao remover o livro.' });
+      }
+    });
+
+
+    
+    
+    // Inicialização do servidor
+    app.listen(port, () => {
+      console.log(`Servidor iniciado em http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+    client.close();
   }
+}
 
-  const query = 'INSERT INTO livros (titulo, autor, ano) VALUES (?, ?, ?)';
-  connection.query(query, [titulo, autor, ano], (error, results) => {
-    if (error) {
-      console.error('Erro ao executar a consulta: ' + error.stack);
-      return res.status(500).json({ error: 'Erro ao inserir o livro.' });
-    }
-
-    return res.status(201).json({ message: 'Livro inserido com sucesso.' });
-  });
-});
-
-
-
-// Rota para buscar um livro
-app.get('/livros/:titulo', (req, res) => {
-  const titulo = req.params.titulo;
-
-  const query = 'SELECT * FROM livros WHERE titulo = ?';
-  connection.query(query, [titulo], (error, results) => {
-    if (error) {
-      console.error('Erro ao executar a consulta: ' + error.stack);
-      return res.status(500).json({ error: 'Erro ao buscar o livro.' });
-    }
-
-    if (results.length === 0) {
-      return res.status(404).json({ error: 'Livro não encontrado.' });
-    }
-
-    return res.json(results[0]);
-  });
-});
-
-
-
-// Rota para remover um livro
-app.delete('/livros/:titulo', (req, res) => {
-  const titulo = req.params.titulo;
-
-  const query = 'DELETE FROM livros WHERE titulo = ?';
-  connection.query(query, [titulo], (error, results) => {
-    if (error) {
-      console.error('Erro ao executar a consulta: ' + error.stack);
-      return res.status(500).json({ error: 'Erro ao remover o livro.' });
-    }
-
-    if (results.affectedRows === 0) {
-      return res.status(404).json({ error: 'Livro não encontrado.' });
-    }
-
-    return res.json({ message: 'Livro removido com sucesso.' });
-  });
-});
-
-
-
-// Inicialização do servidor
-app.listen(port, () => {
-  console.log(`Servidor iniciado em http://localhost:${port}`);
-});
+run().catch(console.error);
--- src/script.js
+++ src/script.js
@@ -1,6 +1,70 @@
-import { adicionarLivro, buscarLivro, removerLivro } from "./backend.js";
 import { BibliotecaManager } from "./BibliotecaManager.js";
 import { Livro } from "./Livro.js";
+
+
+
+//backend.js
+// Função para adicionar um livro no backend
+ async function adicionarLivro(titulo, autor, ano) {
+  try {
+    const response = await fetch('http://localhost:3001/livros', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ titulo, autor, ano })
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error('Erro ao adicionar livro');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+
+// Função para buscar um livro no backend
+ async function buscarLivro(titulo) {
+  try {
+    const response = await fetch(`http://localhost:3001/livros/${titulo}`);
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error('Erro ao buscar livro');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+// Função para remover um livro do backend
+ async function removerLivro(titulo) {
+  try {
+    const response = await fetch(`http://localhost:3001/livros/${titulo}`, {
+      method: 'DELETE'
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error('Erro ao remover livro');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+//
 
 
 
